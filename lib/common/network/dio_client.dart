4eb3e547c8ae85be3184b59a5f5f33043d5ba0ab
Miller Adulu <milleradulu@gmail.com>
2024-07-28 01:41:41 +0300

    Update references to external URLs

--- lib/common/network/dio_client.dart
+++ lib/common/network/dio_client.dart
@@ -1,13 +1,14 @@
 import 'package:dio/dio.dart' show Dio, ResponseType;
 import 'package:fluttercon/common/network/dio_interceptor.dart';
 import 'package:fluttercon/common/utils/constants/api_constants.dart';
+import 'package:fluttercon/common/utils/env/flavor_config.dart';
 import 'package:injectable/injectable.dart';
 
 @lazySingleton
 class DioClient {
   DioClient(this.dio) {
     dio
-      ..options.baseUrl = const String.fromEnvironment('url')
+      ..options.baseUrl = FlutterConConfig.instance!.values.baseUrl
       ..options.headers = ApiConstants.headers
       ..options.connectTimeout = ApiConstants.connectionTimeout
       ..options.receiveTimeout = ApiConstants.receiveTimeout
