a60cc3ef553d305daf01c7b0d665a3b35efd6da3
Miller Adulu <milleradulu@gmail.com>
2024-07-31 15:08:29 +0300

    Refactor auth check into `initState`

--- lib/features/splash/splash_screen.dart
+++ lib/features/splash/splash_screen.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:fluttercon/common/repository/hive_repository.dart';
 import 'package:fluttercon/common/utils/constants/app_assets.dart';
-import 'package:fluttercon/common/utils/env/flavor_config.dart';
 import 'package:fluttercon/common/utils/router.dart';
 import 'package:fluttercon/core/di/injectable.dart';
 import 'package:go_router/go_router.dart';
-import 'package:hive_flutter/hive_flutter.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -25,33 +23,32 @@ class _SplashScreenState extends State<SplashScreen> {
   }
 
   @override
-  Widget build(BuildContext context) {
-    return ValueListenableBuilder(
-      valueListenable:
-          Hive.box<dynamic>(FlutterConConfig.instance!.values.hiveBox)
-              .listenable(),
-      builder: (context, _, __) {
-        final accessToken = getIt<HiveRepository>().retrieveToken();
+  void initState() {
+    final accessToken = getIt<HiveRepository>().retrieveToken();
 
-        if (accessToken == null) {
-          _redirectToPage(
-            context,
-            FlutterConRouter.signInRoute,
-          );
-        } else {
-          _redirectToPage(
-            context,
-            FlutterConRouter.dashboardRoute,
-          );
-        }
-        return const Scaffold(
-          body: Center(
-            child: Image(
-              image: AssetImage(AppAssets.imgDroidcon),
-            ),
-          ),
-        );
-      },
+    if (accessToken == null) {
+      _redirectToPage(
+        context,
+        FlutterConRouter.signInRoute,
+      );
+    } else {
+      _redirectToPage(
+        context,
+        FlutterConRouter.dashboardRoute,
+      );
+    }
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Image(
+          image: AssetImage(AppAssets.imgDroidcon),
+        ),
+      ),
     );
   }
 }
