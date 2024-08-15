87a7fb59c94b2d4a53fed7a2d06162630c167403
NewtonMutugi <newtonmut197@gmail.com>
2024-08-15 15:17:02 +0300

    Refactor OrganisingTeamCard widget to use Wrap instead of Column

--- lib/features/about/widgets/organizing_team_card.dart
+++ lib/features/about/widgets/organizing_team_card.dart
@@ -24,74 +24,63 @@ class _OrganisingTeamCardState extends State<OrganisingTeamCard> {
   Widget build(BuildContext context) {
     final size = MediaQuery.sizeOf(context);
 
-    return SizedBox(
-      height: size.height * 1.5,
-      child: Column(
-        children: [
-          Expanded(
-            child:
-                BlocBuilder<FetchOrganisingTeamCubit, FetchOrganisingTeamState>(
-              builder: (context, state) => state.maybeWhen(
-                loaded: (organisers) => Wrap(
-                  spacing: 10,
+    return BlocBuilder<FetchOrganisingTeamCubit, FetchOrganisingTeamState>(
+      builder: (context, state) => state.maybeWhen(
+        loaded: (organisers) => Wrap(
+          spacing: 10,
+          children: [
+            for (final OrganisingTeam organiser in organisers)
+              SizedBox(
+                width: size.width / 4,
+                child: Column(
                   children: [
-                    for (final OrganisingTeam organiser in organisers)
-                      SizedBox(
-                        width: size.width / 4,
-                        child: Column(
-                          children: [
-                            Container(
-                              decoration: BoxDecoration(
-                                border: Border.all(
-                                  color: ThemeColors.tealColor,
-                                  width: 2,
-                                ),
-                                borderRadius: BorderRadius.circular(10),
-                              ),
-                              child: ClipRRect(
-                                borderRadius: BorderRadius.circular(10),
-                                child: resolveImage(organiser),
-                              ),
-                            ),
-                            const SizedBox(height: 8),
-                            Text(
-                              organiser.name,
-                              style: const TextStyle(
-                                fontWeight: FontWeight.bold,
-                                color: ThemeColors.blueColor,
-                                fontSize: 12,
-                              ),
-                            ),
-                            const SizedBox(height: 4),
-                            Text(
-                              organiser.tagline,
-                              style: TextStyle(
-                                color: Colors.grey[600],
-                                fontSize: 9,
-                              ),
-                              textAlign: TextAlign.center,
-                            ),
-                          ],
+                    Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: ThemeColors.tealColor,
+                          width: 2,
                         ),
+                        borderRadius: BorderRadius.circular(10),
                       ),
-                  ],
-                ),
-                error: (message) => Text(
-                  message,
-                  style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: resolveImage(organiser),
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    Text(
+                      organiser.name,
+                      style: const TextStyle(
                         fontWeight: FontWeight.bold,
                         color: ThemeColors.blueColor,
-                        fontSize: 18,
+                        fontSize: 12,
                       ),
-                ),
-                orElse: () => const Center(
-                  child: CircularProgressIndicator(),
+                    ),
+                    const SizedBox(height: 4),
+                    Text(
+                      organiser.tagline,
+                      style: TextStyle(
+                        color: Colors.grey[600],
+                        fontSize: 9,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
                 ),
               ),
-            ),
-          ),
-          const Spacer(),
-        ],
+          ],
+        ),
+        error: (message) => Text(
+          message,
+          style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                fontWeight: FontWeight.bold,
+                color: ThemeColors.blueColor,
+                fontSize: 18,
+              ),
+        ),
+        orElse: () => const Center(
+          child: CircularProgressIndicator(),
+        ),
       ),
     );
   }
