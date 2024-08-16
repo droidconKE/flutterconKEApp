56aca7b691978118a9c837722acfe97154c11290
NewtonMutugi <newtonmut197@gmail.com>
2024-08-16 12:41:23 +0300

    Removed need for flutter_svg_provider dependacy

--- lib/features/feedback/widgets/feedback_custom_appbar.dart
+++ lib/features/feedback/widgets/feedback_custom_appbar.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg_provider/flutter_svg_provider.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:fluttercon/common/utils/constants/app_assets.dart';
 import 'package:fluttercon/features/feedback/widgets/back_button.dart';
 
@@ -9,34 +9,39 @@ class FeedbackCustomAppBar extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
 
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10.0 * 3),
-      decoration: const BoxDecoration(
-        image: DecorationImage(
-          image: Svg(AppAssets.feedbackBanner),
-          fit: BoxFit.cover,
-        ),
-      ),
-      child: Row(
-        // crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const SizedBox(height: 10),
-          backButton(context),
-          const SizedBox(width: 10),
-          Padding(
-            padding: const EdgeInsets.all(18),
-            child: Row(
-              children: [
-                Text(
-                  'Feedback',
-                  style: theme.textTheme.headlineMedium
-                      ?.copyWith(color: Colors.white, fontSize: 20),
-                ),
-              ],
-            ),
+    return Stack(
+      children: [
+        Positioned.fill(
+          child: SvgPicture.asset(
+            AppAssets.feedbackBanner,
+            fit: BoxFit.cover,
           ),
-        ],
-      ),
+        ),
+        Container(
+          padding:
+              const EdgeInsets.symmetric(horizontal: 10, vertical: 10.0 * 3),
+          child: Row(
+            // crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 10),
+              backButton(context),
+              const SizedBox(width: 10),
+              Padding(
+                padding: const EdgeInsets.all(18),
+                child: Row(
+                  children: [
+                    Text(
+                      'Feedback',
+                      style: theme.textTheme.headlineMedium
+                          ?.copyWith(color: Colors.white, fontSize: 20),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
