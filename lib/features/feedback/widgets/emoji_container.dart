61301ab6d0349b45e4f132e91688ece174ad25d9
NewtonMutugi <newtonmut197@gmail.com>
2024-08-16 10:31:11 +0300

    Add EmojiContainer widget for displaying emojis in feedback screen

--- lib/features/feedback/widgets/emoji_container.dart
+++ lib/features/feedback/widgets/emoji_container.dart
@@ -2,20 +2,43 @@ import 'package:flutter/material.dart';
 import 'package:fluttercon/core/theme/theme_colors.dart';
 
 class EmojiContainer extends StatelessWidget {
-  const EmojiContainer({super.key, this.path});
-  final String? path;
+  const EmojiContainer({
+    super.key,
+    required this.path,
+    this.onTap,
+    this.isSelected = false,
+  });
+
+  final String path;
+  final void Function()? onTap;
+  final bool isSelected;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(20),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-        color: ThemeColors.lightGrayColor,
-      ),
-      child: Image.asset(
-        path!,
-        width: 40,
-        height: 40,
+    return GestureDetector(
+      onTap: onTap,
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 200),
+        padding: const EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: isSelected
+              ? ThemeColors.blueDroidconColor
+              : ThemeColors.lightGrayColor,
+          boxShadow: [
+            if (onTap != null)
+              BoxShadow(
+                color: Colors.black.withOpacity(0.5),
+                blurRadius: 4,
+                offset: const Offset(0, 2),
+              ),
+          ],
+        ),
+        child: Image.asset(
+          path,
+          width: 40,
+          height: 40,
+        ),
       ),
     );
   }
