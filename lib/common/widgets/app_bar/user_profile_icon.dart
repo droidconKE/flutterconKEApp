9794fde52a44c2b7f18e9ef4cd6232ebe9bbb3c6
Miller Adulu <milleradulu@gmail.com>
2024-08-16 15:40:36 +0300

    Add profile icon on the nav bar

--- lib/common/widgets/app_bar/user_profile_icon.dart
+++ lib/common/widgets/app_bar/user_profile_icon.dart
@@ -1,6 +1,10 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:fluttercon/common/widgets/bottom_nav/app_nav_icon.dart';
+import 'package:fluttercon/common/repository/hive_repository.dart';
+import 'package:fluttercon/common/utils/env/flavor_config.dart';
+import 'package:fluttercon/core/di/injectable.dart';
 import 'package:fluttercon/core/theme/theme_colors.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 class UserProfileIcon extends StatefulWidget {
   const UserProfileIcon({super.key});
@@ -22,7 +26,27 @@ class _UserProfileIconState extends State<UserProfileIcon> {
           shape: BoxShape.circle,
           color: ThemeColors.tealColor,
         ),
-        child: const AppNavIcon('locked', color: Colors.white, height: 15),
+        child: ValueListenableBuilder<Object>(
+          valueListenable:
+              Hive.box<dynamic>(FlutterConConfig.instance!.values.hiveBox)
+                  .listenable(),
+          builder: (context, _, __) {
+            final profile = getIt<HiveRepository>().retrieveUser();
+            if (profile != null) {
+              return ClipOval(
+                child: CachedNetworkImage(
+                  imageUrl: profile.avatar,
+                  height: 100,
+                  width: 100,
+                ),
+              );
+            }
+            return const Icon(
+              Icons.person,
+              color: Colors.white,
+            );
+          },
+        ),
       ),
     );
   }
