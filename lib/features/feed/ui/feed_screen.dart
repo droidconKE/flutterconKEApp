3358d4ce434d8aa130b581b1a524993174e98508
Miller Adulu <milleradulu@gmail.com>
2024-11-05 14:49:22 +0300

    Move static strings to an arb file

--- lib/features/feed/ui/feed_screen.dart
+++ lib/features/feed/ui/feed_screen.dart
@@ -43,7 +43,7 @@ class _FeedScreenState extends State<FeedScreen> {
             loaded: (fetchedFeeds) => fetchedFeeds.isEmpty
                 ? Center(
                     child: Text(
-                      'No posts yet',
+                      l10n.noPosts,
                       style: TextStyle(
                         color: colorScheme.onSurface,
                         fontSize: 20,
