e7916c7f062adfef082b92815cda04ad429910dd
NewtonMutugi <newtonmut197@gmail.com>
2026-07-15 14:37:44 +0300

    style: update switch track color to flutterconBlue in sessions screen

--- lib/features/sessions/ui/sessions_screen.dart
+++ lib/features/sessions/ui/sessions_screen.dart
@@ -127,11 +127,12 @@ class _SessionsScreenState extends State<SessionsScreen>
                                   },
                                   trackOutlineWidth: WidgetStateProperty.all(1),
                                   trackColor: WidgetStateProperty.all(
-                                    Colors.black,
+                                    ThemeColors.flutterconBlue,
                                   ),
                                   activeTrackColor:
                                       ThemeColors.flutterconMagenta,
-                                  activeThumbColor: ThemeColors.flutterconMagenta,
+                                  activeThumbColor:
+                                      ThemeColors.flutterconMagenta,
                                   thumbColor: WidgetStateProperty.all(
                                     Colors.white,
                                   ),
